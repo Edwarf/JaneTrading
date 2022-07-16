308a87d3ae0da2bae8856424c1d838bf2646839f
Edward Frazer <edward95135@gmail.com>
2022-07-16 16:18:28 -0700

    okay

--- main.py
+++ main.py
@@ -271,14 +271,14 @@ def main():
             vale_fair_value = (vale_bid + vale_ask)//2
             valbz_bid, valbz_ask = market_book.best_price_both("VALBZ")
             valbz_fair_value = (valbz_ask + valbz_bid)//2
-            if valbz_fair_value - vale_fair_value > 25:
+            if valbz_fair_value - vale_fair_value > 12:
                 exchange.send_add_message(Ledger.current_id, "VALBZ", Dir.SELL, 1, valbz_bid-1)
                 exchange.send_add_message(Ledger.current_id, "VALE", Dir.BUY, 1, vale_ask+1)
-                exchange.send_convert_message(Ledger.current_id, "VALE", Dir.SELL, 1)
-            elif vale_fair_value - valbz_fair_value > 25:
+                # exchange.send_convert_message(Ledger.current_id, "VALE", Dir.SELL, 1)
+            elif vale_fair_value - valbz_fair_value > 12:
                 exchange.send_add_message(Ledger.current_id, "VALE", Dir.SELL, 1, vale_bid-1)
                 exchange.send_add_message(Ledger.current_id, "VALBZ", Dir.BUY, 1, valbz_ask+1)
-                exchange.send_convert_message(Ledger.current_id, "VALE", Dir.BUY, 1)
+                # exchange.send_convert_message(Ledger.current_id, "VALE", Dir.BUY, 1)
 
 
 
